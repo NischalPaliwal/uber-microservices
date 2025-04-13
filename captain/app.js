bc643d96e1f740e80b5eaa936e1ed5275c4ac79b
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-04-13 20:08:07 +0530

    code updated

--- captain/app.js
+++ captain/app.js
@@ -3,7 +3,7 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const connectToDB = require('./db/db');
-const userRoutes = require('./routes/captain.routes');
+const captainRoutes = require('./routes/captain.routes');
 const cookieParser = require('cookie-parser');
 const rabbitMQ = require('./service/rabbit');
 
@@ -14,6 +14,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', userRoutes);
+app.use('/', captainRoutes);
 
 module.exports = app;
